aa44d6aa41e4e4d02d0744ace1e2c5d37af1ec40
Marlom Souza <marlom.souza@before.com.br>
2018-02-08 13:13:14 -0300

    Adicionado tela para cadastrar

--- modulo3/Controllers/ProdutoController.cs
+++ modulo3/Controllers/ProdutoController.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using modulo3.Models;
 
 namespace modulo3.Controllers
 {
     [Route("api/produto")]
     public class ProdutoController : Controller
     {
-        [HttpGet("{id:int}")]
-        public int get(int id){
-            return id;
+        public IActionResult Index()
+        {
+            return View();
         }
 
+        [HttpGet("{id:int}")]
+        public string get(int id)
+        {
+            return HttpContext.Request.Path + id;
+        }
+
+        [HttpPost]
+        public string Post(Produto produto)
+        {
+            return produto.Nome;
+        }
     }
 }
