7e5594a415c3a7ca6f780f50bf6d28ae2812bc39
Marlom Souza <thunder_bolt2000@hotmail.com>
2018-02-11 22:05:37 -0300

    Criando contexto

---
+++ modulo4/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using modulo4.Domain;
+
+namespace modulo4.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
