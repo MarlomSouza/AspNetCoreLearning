b57ce0c9d6231bbe4ffbe988a002aca037e7f8d2
Marlom Souza <marlom.souza@before.com.br>
2018-02-21 13:08:00 -0400

    Classes de dominio

---
+++ modulo7/src/MarlomStore.Domain/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+namespace MarlomStore.Domain.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ modulo7/src/MarlomStore.Domain/Products/CategoryStore.cs
@@ -0,0 +1,27 @@
+using MarlomStore.Domain.Dtos;
+using MarlomStore.Domain.Repository;
+
+namespace MarlomStore.Domain.Products
+{
+    public class CategoryStore
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public CategoryStore(IRepository<Category> categoryRepository) => _categoryRepository = categoryRepository;
+
+
+        public void Store(CategoryDto categoryDto)
+        {
+            var category = _categoryRepository.Get(categoryDto.Id);
+
+            if (category == null)
+            {
+                category = new Category(categoryDto.Name);
+                _categoryRepository.Save(category);
+            }
+            else
+                category.Update(categoryDto.Name);
+
+        }
+    }
+}
